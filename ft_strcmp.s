61699d913fd5350d3bb980dfeb00cc97804981e6
kohkubo <kohkubo@student.42tokyo.jp>
2021-04-14 09:56:02 +0900

    fix: rbpレジスタの使用方法について間違っていたため、訂正。また、cmpsbを使用するにしてコードを簡潔にした。

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -2,18 +2,13 @@ section .text
 	global _ft_strcmp
 
 _ft_strcmp:
-	xor		eax, eax
-	xor		ebx, ebx
-	xor		ecx, ecx
 .loop:
-	mov		al, BYTE[rdi + rcx]
-	mov		bl, BYTE[rsi + rcx]
-	cmp		al, 0
-	je		.end
-	cmp		al, bl
+	movsx	eax, BYTE[rdi]
+	cmpsb
 	jne		.end
-	inc		rcx
-	jmp		.loop
+	test	al, al
+	jne		.loop
 .end:
-	sub		eax, ebx
+	movsx	ecx, BYTE [rsi - 1]
+	sub		eax, ecx
 	ret
